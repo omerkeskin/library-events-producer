caffd89603171c750135261e8d1b7f512d2919f8
Ömer Keskin <omer.keskin.deu@hotmail.com>
2022-01-29 21:14:44 +0300

    PUT producer

--- src/main/java/com/omerkeskin/libraryeventsproducer/controller/LibraryEventsController.java
+++ src/main/java/com/omerkeskin/libraryeventsproducer/controller/LibraryEventsController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +35,25 @@ public class LibraryEventsController {
         //log.info("Send result {} ", sendResult.toString());
         log.info("After postLibraryEvent ");
         return ResponseEntity.status(HttpStatus.CREATED).body(libraryEvent);
+    }
+
+
+    @PutMapping("v1/libraryevent")
+    public ResponseEntity<?> putLibraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException, ExecutionException, InterruptedException, TimeoutException {
+
+        if(libraryEvent.getLibraryEventId() == null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Please pass the LibraryEventId");
+        }
+
+        // invoke kafka producer
+        log.info("Before putLibraryEvent ");
+        //libraryEventProducer.sendLibraryEvent(libraryEvent);
+        libraryEvent.setLibraryEventType(LibraryEventType.UPDATE);
+        libraryEventProducer.sendLibraryEventApproach2(libraryEvent);
+        //SendResult<Integer, String> sendResult = libraryEventProducer.sendLibraryEventSync(libraryEvent);
+        //log.info("Send result {} ", sendResult.toString());
+        log.info("After putLibraryEvent ");
+        return ResponseEntity.status(HttpStatus.OK).body(libraryEvent);
     }
 
 }
